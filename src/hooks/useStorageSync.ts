04cc9e77f02fb0966f4bf74958ce1506c5b19f5f
tangxianwen <1658172479@qq.com>
2022-04-10 23:27:24 +0800

    feat: 添加storage hook

--- src/hooks/useStorageSync.ts
+++ src/hooks/useStorageSync.ts
@@ -1,10 +1,60 @@
-export default function useStorageSync(key, data) {
+import type { MaybeRef } from "@/utils/basic";
+import { ref } from "vue";
+export interface Serializer {
+  getAll(): void;
+  clear(): void;
+}
+
+export type ReturnType<T> = Serializer & {
+  data: MaybeRef<T>;
+};
+export default function useStorageSync(): any | void;
+export default function useStorageSync(
+  key: string,
+  initialValue?: MaybeRef<string>
+): ReturnType<string>;
+export default function useStorageSync(
+  key: string,
+  initialValue?: MaybeRef<number>
+): ReturnType<number>;
+export default function useStorageSync(
+  key: string,
+  initialValue?: MaybeRef<boolean>
+): ReturnType<boolean>;
+export default function useStorageSync<T>(
+  key: string,
+  initialValue?: MaybeRef<T>
+): ReturnType<T>;
+export default function useStorageSync<T = unknown>(
+  key: string,
+  initialValue?: MaybeRef<null>
+): ReturnType<T>;
+export default function useStorageSync<
+  T extends string | number | boolean | object | null
+>(key?: string, initialValue?: MaybeRef<T>): ReturnType<T> {
+  const data = ref(initialValue);
+
+  if (key) {
+    write(data);
+  }
   function write(v: unknown) {
     if (!v) {
-      uni.removeStorageSync(key);
+      uni.removeStorageSync(key!);
     } else {
-      uni.setStorageSync(key, v);
+      uni.setStorageSync(key!, v);
     }
   }
-  write(data);
+
+  function getAll() {
+    return uni.getStorageInfoSync();
+  }
+
+  function clear() {
+    uni.clearStorageSync();
+  }
+  return {
+    data,
+    getAll,
+    clear,
+  };
 }
--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -1,3 +1,6 @@
+import type { Ref } from "vue";
 export type Numeric = number | string;
 
 export type AnyFunc = (...args: any[]) => any;
+
+export type MaybeRef<T> = T | Ref<T>;
