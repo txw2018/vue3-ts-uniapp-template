eb2237052bd1ceb5cc2855a9025b7b1ffb232c21
txw <1658172479@qq.com>
2022-04-23 08:50:47 +0800

    feat: 去掉无用文件

--- src/hooks/useStorage.ts
+++
@@ -1,9 +0,0 @@
-export default function useStorage(key, data, success, fail, complete) {
-  uni.setStorage({
-    key: key,
-    data: data,
-    success: success,
-    fail: fail,
-    complete: complete,
-  });
-}
